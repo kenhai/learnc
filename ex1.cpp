45689e730f7d2fd275bd6c15fa257705fade210b
kenhai <570583687hk@gmail.com>
2020-01-06 11:45:48 +0800

    use c++ print hello

---
+++ ex1.cpp
@@ -0,0 +1,14 @@
+//#include "stdafx.h"
+//#include <cstdio>
+//#include <cwchar>
+//#include <opt/local/include/gcc6/c++/tr1/wchar>
+#include <iostream>
+//#include <wchar.h>
+//#include </opt/local/include/gcc7/c++/tr1/wchar.h>
+using namespace std;
+
+int main()
+{
+	cout<<"hello\n";
+	return 0;
+}

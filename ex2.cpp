dda5038a144fbfa9a9757a553b6d14c33cb6b951
kenhai <570583687hk@gmail.com>
2020-01-07 17:33:29 +0800

    practice cin and cout

---
+++ ex2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int radius(0);
+  cout<<"Please enter the radius!\n";
+  cin>>radius;
+  cout<<"The radius is:"<<radius<<'\n';
+  cout<<"Please enter a different radius!\n";
+  cin>>radius;
+  cout<<"Now the radius is changed to:"<<radius<<'\n';
+  return 0;
+}
+

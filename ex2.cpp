50ca9a8b0c4fab2f48c4bbfae4c3231acb5e1fa0
kenhai <570583687hk@gmail.com>
2020-01-07 17:41:50 +0800

    and const variables

--- ex2.cpp
+++ ex2.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int main()
 {
+  const double pi(3.14159);
   int radius(0);
   cout<<"Please enter the radius!\n";
   cin>>radius;
   cout<<"The radius is:"<<radius<<'\n';
+  cout<<"PI is:"<<pi<<'\n'
   cout<<"Please enter a different radius!\n";
   cin>>radius;
   cout<<"Now the radius is changed to:"<<radius<<'\n';
